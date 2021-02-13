75221a53772de9e0c26092e981e8d3a1d38860ef
jjshaws <63075892+jjshaws@users.noreply.github.com>
2021-02-13 04:27:41 -0800

    Tests finished and documented v2

--- src/test/model/HabitTrackerTest.java
+++ src/test/model/HabitTrackerTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 public class HabitTrackerTest {
     HabitTracker app;
 
